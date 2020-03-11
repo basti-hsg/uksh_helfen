596474a928d41ee9d2b98c9620a515b6ac1ac59b
Sebastian Friedersen <sebastian.friedersen@hammelsprung.com>
2020-03-11 15:56:43 +0100

    set scroll and focus for form

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import "@fortawesome/fontawesome-free/css/all.css";
 
 import "./styles/style.scss";
 // js libs
-import "alpinejs";
+// import "alpinejs";
+
+// add eventlistener to autofocus form input after cta click
+const subscribe_cta_elements = document.querySelectorAll(".subscribe-cta");
+const input_name_element = document.getElementById("name-input");
+
+subscribe_cta_elements.forEach(function(element) {
+  element.addEventListener("click", function() {
+    window.setTimeout(function() {
+      input_name_element.focus();
+    }, 0);
+  });
+});
