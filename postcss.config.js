e96df15faf29bb51c0a452a6de32859ad3a92c3a
SebastianFriedersen <sebastian.friedersen@scc-events.com>
2020-03-17 21:01:00 +0100

    initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,3 @@
-// postcss.config.js
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  // Specify the paths to all of the template files in your project
-  content: ["./src/**/*.html"],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
-
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    // remove this on developemnt
-    purgecss
-    // ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  ]
+  plugins: [require("autoprefixer")]
 };
