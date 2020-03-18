88c983a2544bf6fbd1cd2c50c1c0ca6f0a7a90bf
SebastianFriedersen <sebastian.friedersen@scc-events.com>
2020-03-18 14:57:35 +0100

    inital commit again

--- src/index.js
+++ src/index.js
@@ -2,28 +2,4 @@ import "normalize.css";
 
 import "./styles/style.scss";
 // js libs
-// import "alpinejs";
-
-// add eventlistener to autofocus form input after cta click
-const subscribe_cta_elements = document.querySelectorAll(".subscribe-cta");
-const input_name_element = document.getElementById("name-input");
-
-subscribe_cta_elements.forEach(function(element) {
-  element.addEventListener("click", function() {
-    window.setTimeout(function() {
-      input_name_element.focus();
-    }, 0);
-  });
-});
-
-// keep labels up after text is entered in input
-const form_text_input_elements = document.querySelectorAll("form input");
-form_text_input_elements.forEach(function(input, index) {
-  input.addEventListener("focusout", function() {
-    if (input.value) {
-      input.classList.add("filled");
-    } else {
-      input.classList.remove("filled");
-    }
-  });
-});
+import "alpinejs";
